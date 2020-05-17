9fe77e5b975a52434d80ea1a27039597844b71f5
Dlama istifanus <dlamaistifanus0@gmail.com>
2020-05-17 19:39:05 +0100

    side-menu

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 
 var menu = document.querySelector('.fa-bars'),
-    cancel = document.querySelector('.fa-times')
+    cancel = document.querySelector('.fa-times'),
+    links = document.querySelector('.menu');
 menu.addEventListener('click', function(){
-    var links = document.querySelector('.menu');
     links.style.display = 'block'
     menu.style.display = 'none'
     cancel.style.display = 'block'
 })
 
 cancel.addEventListener('click', function(){
-    var links = document.querySelector('.menu');
     links.style.display = 'none'
     menu.style.display = 'block'
     cancel.style.display = 'none'
